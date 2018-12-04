683910a3a6e829b6efe922043e56554b61598b6f
Joe Wood <joe.wood@bbc.co.uk>
2018-12-04 18:36:43 +0000

    Refactor out list algorythm logic out of request function

--- article/retrieve_article.py
+++ article/retrieve_article.py
@@ -1,16 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-def retrieve_news_article_as_list(article_url):
-    class_name = 'story-body__inner'
 
+def split_list_of_sentences_into_list_of_words(list_of_sentences):
+    return [sentences.split() for sentences in list_of_sentences]
+
+
+def dump_news_article_main_body(article_url, body_class_name):
     r = requests.get(article_url)
 
     soup = BeautifulSoup(r.content, 'html.parser')
 
-    main_text = soup.find("div", {"class": class_name}).findAll('p')
+    main_text = soup.find("div", {"class": body_class_name}).findAll('p')
 
-    return [words for sentance in main_text for words in sentance.get_text().split()]
+    return [sentence.get_text() for sentence in main_text]
 
 
 def list_news_articles_as_urls(base_url, news_site_suffix='news'):
--- tests/test_retrieve_article.py
+++ tests/test_retrieve_article.py
@@ -1,10 +1,7 @@
-import pytest
-
 from article import retrieve_article
 
 
 def test_list_news_articles_as_urls():
-
     url = "https://www.bbc.co.uk/"
 
     url_list = retrieve_article.list_news_articles_as_urls(url)
@@ -16,12 +13,25 @@ def test_list_news_articles_as_urls():
 
 def test_retrieve_news_article_as_list():
 
+    list_of_sentences = [
+        'The UK should be able to unilaterally cancel its withdrawal from the EU',
+        'anther test sentence',
+        'here to another year of a sentence'
+     ]
+    word_list = retrieve_article.split_list_of_sentences_into_list_of_words(list_of_sentences)
+
+    assert type(word_list) is list
+
+
+def test_it_can_dump_news_article_main_body():
     url = "https://www.bbc.co.uk/"
 
     article_suffix = retrieve_article.list_news_articles_as_urls(url)
 
     article_url = 'https://www.bbc.co.uk/' + article_suffix[0]
-    print(article_url)
-    news_string = retrieve_article.retrieve_news_article_as_list(article_url)
 
+    class_name = 'story-body__inner'
+
+    news_string = retrieve_article.dump_news_article_main_body(article_url, class_name)
+    print(news_string)
     assert type(news_string) is list
